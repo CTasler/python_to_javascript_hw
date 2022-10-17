d23e07a051fc3c38a7a750f78118fb93889aa2c4
Claire Tasler <tasl0368@gmail.com>
2022-10-17 18:51:17 -0500

    Initial commit

---
+++ melonCounting.js
@@ -0,0 +1,28 @@
+'use strict';
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+function countMelons(melons){
+    const melonCount = {};
+    for (const melon of melons){
+        if (melon in melonCount){
+            melonCount[melon] += 1;
+        }
+        else{
+            melonCount[melon] = 1;
+        }
+    }
+    return melonCount;
+}
+
+
+console.log(countMelons(melonsToAdd));
